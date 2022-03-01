d9a846bdfe3f0f4f64bdc1ec86af79bd85272433
Linus Wallgren <linus@nectarinehealth.com>
2022-03-01 11:02:15 +0100

    fixup

--- reproduce.sh
+++ reproduce.sh
@@ -7,8 +7,8 @@ git checkout master
 git checkout -b feature_1
 git commit --allow-empty --message "first commit"
 git push origin feature_1
-hub pull-request --base="master"
+hub pull-request --base="master" --message "First PR"
 git checkout -b feature_2
-git commit --allow-empty --message "second commit"
+git commit --allow-empty --message "second commit" --message "Second PR"
 git push origin feature_2
 hub pull-request --base "feature_1"
