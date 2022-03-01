576c09c4d588cc3d92bd512420e2a1cbd0a99166
Linus Wallgren <linus@nectarinehealth.com>
2022-03-01 10:59:50 +0100

    Add script to reproduce issue

---
+++ reproduce.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+git checkout master
+git checkout -b feature_1
+git commit --allow-empty --mesage "first commit"
+git push origin feature_1
+hub pull-request --base="master"
+git checkout -b feature_2
+git commit --allow-empty --message "second commit"
+git push origin feature_2
+hub pull-request --base "feature_1"
